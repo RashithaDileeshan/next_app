7a703e17884104a65cf7508a99beef59a5ed8aaa
Rashitha Dileeshan <rashbatugedara9@gmail.com>
2022-12-30 19:58:31 +0530

    change

--- pages/singlePage.tsx
+++ pages/singlePage.tsx
@@ -1,23 +1,36 @@
 import React, { useEffect, useState } from 'react'
-
+import axios from 'axios'
 
 const SinglePage = () => {
-    
+
+    const [data, setData] = useState([{ id: 0, title: '', url: '' }])
+
+    const getData = () => {
+        axios
+            .get('https://jsonplaceholder.typicode.com/photos', {
+            })
+            .then((response) => {
+                if (response.status == 200) {
+                    setData(response.data)
+                }
+            })
+            .catch((error: any) => {
+                console.log(error)
+            })
+    }
+
+    useEffect(() => {
+        getData()
+    }, [])
+
     return (
         <>
-            <div className='row'>
-                <div className='col-md-3'>
-                    <img className="card-text" src={'https://via.placeholder.com/600/92c952'} style={{ width: '100%' }} />
-                </div>
-                <div className='col-md-3'>
-                    <img className="card-text" src={'https://via.placeholder.com/600/92c952'} style={{ width: '100%' }} />
-                </div>
-                <div className='col-md-3'>
-                    <img className="card-text" src={'https://via.placeholder.com/600/92c952'} style={{ width: '100%' }} />
-                </div>
-                <div className='col-md-3'>
-                    <img className="card-text" src={'https://via.placeholder.com/600/92c952'} style={{ width: '100%' }} />
-                </div>
+            <div className='row' style={{ marginBottom: '20px' }}>
+                {data.map((item) => (
+                    <div className='col-md-3'>
+                        <img className="card-text" src={item.url} style={{ width: '100%' }} />
+                    </div>
+                ))}
             </div>
         </>
     )
